6913b32682314b7f300d1c66603c880573fc3c24
Dani Matuko <matuko305@gmail.com>
2023-06-08 11:45:26 +0300

    init and add function to functions.php

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+
+
+require 'lib/helpers.php';
---
+++ lib/helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Display the post meta information.
+ *
+ * @return void
+ */
+function ascendia_post_meta() {
+    $date = get_the_date();
+    $author = get_the_author();
+    $authorURL = get_author_posts_url(get_the_author_meta("ID"));
+
+    echo
+    <<<POSTMETA
+        Posted on <time class="post-date">$date</time> by
+        <a href="$authorURL">$author</a>
+    POSTMETA;
+}
+
+
+
+/**
+ * Display the "Read More" link with title attribute.
+ *
+ * @return void
+ */
+function ascendia_read_more_link() {
+    $permalink = get_the_permalink();
+    $title = get_the_title();
+    $titleAttribute = the_title_attribute(['echo' => false]);
+
+    echo
+    <<<READ_MORE
+    <a href="$permalink" title="$titleAttribute">
+        Read More <span class="u-screen-reader-text">about $title</span>
+    </a>
+    READ_MORE;
+}
