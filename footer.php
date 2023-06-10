63dbca5b3a0d2fb5e0094fdfae21d7f7d89e0a0e
Dani Matuko <matuko305@gmail.com>
2023-06-10 13:05:54 +0300

    create footer sidebar dynamiclly

--- footer.php
+++ footer.php
@@ -1,8 +1,32 @@
 </main>
 
 <footer>
-    <!-- Add your footer content here -->
-    <p>&copy; <?php echo date('Y'); ?> My Theme. All rights reserved.</p>
+    <div class="container is-fluid">
+
+        <?php $footer_layout = "3,3,3,3";
+        $columns = array_map('intval', explode(",", $footer_layout));
+        $footer_bg = "dark";
+
+        ?>
+        <div class="columns">
+
+            <?php foreach ($columns as $i => $column) : ?>
+
+                <?php if (is_active_sidebar('footer-widget-' . $i + 1)) : ?>
+                    <section id="sidebar" class="widget-area column" <?php dynamic_sidebar('footer-widget-' . $i + 1); ?> </section>
+                    <?php endif; ?>
+
+                <?php endforeach; ?>
+
+        </div>
+
+    </div>
+
+    <p class="has-text-centered has-text-light has-background-black	py-3 mt-5">
+        &copy; <?php echo date('Y'); ?> My Theme.
+        All rights reserved.
+    </p>
+
 </footer>
 
 <?php wp_footer(); ?>
--- lib/sidebars.php
+++ lib/sidebars.php
@@ -1,16 +1,58 @@
 <?php
 
-function ascendia_register_sidebar() {
-    register_sidebar(
-        array(
-            'name'          => __('Primary Sidebar', 'ascendia'),
-            'id'            => 'primary-sidebar',
-            'description'   => __('This primary sidbar, appears in the blog page', 'ascendia'),
+/**
+ * Register footer widgets for the Ascendia theme.
+ * 
+ * @return void
+ */
+function ascendia_register_footer_widgets() {
+    $footer_layout = "3,3,3,3";
+    $columns = array_map('intval', explode(",", $footer_layout));
+    $footer_bg = "dark";
+
+    foreach ($columns as $i => $column) {
+        $args =  array(
+            'id'            => 'footer-widget-' . ($i + 1),
+            'name'          => sprintf(__('Footer Widget %d', 'ascendia'), ($i + 1)),
+            'description'   => __('This primary sidebar appears in the blog page', 'ascendia'),
             'before_widget' => '<section id="%1$s" class="widget %2$s pl-4">',
             'after_widget'  => '</section>',
             'before_title'  => '<h2 class="widget-title is-size-4 has-text-weight-bold mb-1">',
             'after_title'   => '</h2>',
-        )
-    );
+        );
+
+        register_sidebar($args);
+    }
 }
-add_action('widgets_init', 'ascendia_register_sidebar');
+
+/**
+ * Register the primary sidebar for the Ascendia theme.
+ *
+ * @return void
+ */
+function ascendia_register_primary_sidebar() {
+    $args = array(
+        'name'          => __('Primary Sidebar', 'ascendia'),
+        'id'            => 'primary-sidebar',
+        'description'   => __('This primary sidebar appears in the blog page', 'ascendia'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s pl-4">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title is-size-4 has-text-weight-bold mb-1">',
+        'after_title'   => '</h2>',
+    );
+
+    register_sidebar($args);
+}
+
+/**
+ * Register all sidebars for the Ascendia theme.
+ *
+ * @return void
+ */
+function ascendia_register_sidebars() {
+    ascendia_register_primary_sidebar();
+    ascendia_register_footer_widgets();
+}
+
+// Hook into the widgets_init action to register sidebars
+add_action('widgets_init', 'ascendia_register_sidebars');
