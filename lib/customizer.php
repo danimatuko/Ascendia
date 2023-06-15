ba795f58f8f9a73c1a46a80c134f19eabb8423c7
Dani Matuko <matuko305@gmail.com>
2023-06-15 12:48:02 +0300

    sanitize footer info with custom function

--- lib/customizer.php
+++ lib/customizer.php
@@ -14,7 +14,7 @@ function ascendia_customize_register($wp_customize) {
     // Add a setting for site info
     $wp_customize->add_setting('ascendia_site_info', array(
         'default' => 'Ascendia theme, All rights reserved.',
-        'sanitize_callback' => 'sanitize_text_field',
+        'sanitize_callback' => 'sanitize_html_except_links',
     ));
 
     // Add a control for site info
@@ -26,3 +26,29 @@ function ascendia_customize_register($wp_customize) {
 }
 
 add_action('customize_register', 'ascendia_customize_register');
+
+
+
+
+/**
+ * Sanitize HTML content while preserving links
+ * Allow <a> tags with href attribute and target attribute
+ * Remove all other HTML tags and attributes
+ *
+ * @param string $html The HTML content to sanitize
+ * @return string Sanitized HTML content
+ */
+function sanitize_html_except_links($html) {
+    // Define the allowed HTML tags and attributes
+    $allowed_tags = array(
+        'a' => array(
+            'href' => true,
+            'title' => true
+        ),
+    );
+
+    // Remove all disallowed HTML tags and attributes except for links
+    $sanitized_html = wp_kses($html, $allowed_tags);
+
+    return $sanitized_html;
+}
--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -1,9 +1,14 @@
 <?php
 
 $site_info = get_theme_mod('ascendia_site_info');
-if (!$site_info) return;
 
+if (!$site_info) return;
 ?>
+
+
 <p class="text-light py-3 mt-5">
-    <?php echo esc_html($site_info); ?>
+    <?php
+    // data is secured by the customizer(ascendia_customize_register)
+    echo ($site_info);
+    ?>
 </p>
